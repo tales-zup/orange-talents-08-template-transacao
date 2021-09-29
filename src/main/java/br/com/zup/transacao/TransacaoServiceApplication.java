0b8397ea14661f8633c3afd6bfaff24b7ca9774d
tales-zup <tales.araujo@zup.com.br>
2021-09-28 22:15:12 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/transacao/TransacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoServiceApplication.class, args);
+	}
+
+}
